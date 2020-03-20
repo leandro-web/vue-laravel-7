eec59027238b4aac5ab7c5d22f97779fd9ffcf72
leandro-web <leandro-web@hotmail.com>
2020-03-20 18:33:47 -0300

    Tabela de Lista de Artigos

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -3,8 +3,57 @@
 @section('content')
     <pagina tamanho="12">
         <painel titulo="Lista de artigos">
-            Teste de conteúdo...
             
+            <a href="#" class="btn btn-success my-3">Criar</a>
+
+            <table class="table table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Título</th>
+                        <th scope="col">Descrição</th>
+                        <th scope="col">Autor</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody >
+                    <tr>
+                        <td>1</td>
+                        <td>Título...</td>
+                        <td>Descrição...</td>
+                        <td>Leandro</td>
+                        <td>20/03/2020</td>
+                        <td>
+                            <a href="#" class="btn btn-outline-primary btn-sm mx-1">Editar</a>
+                            <a href="#" class="btn btn-outline-danger btn-sm mx-1">Deletar</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Título...</td>
+                        <td>Descrição...</td>
+                        <td>Leandro</td>
+                        <td>20/03/2020</td>
+                        <td>
+                            <a href="#" class="btn btn-outline-primary btn-sm mx-1">Editar</a>
+                            <a href="#" class="btn btn-outline-danger btn-sm mx-1">Deletar</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Título...</td>
+                        <td>Descrição...</td>
+                        <td>Leandro</td>
+                        <td>20/03/2020</td>
+                        <td>
+                            <a href="#" class="btn btn-outline-primary btn-sm mx-1">Editar</a>
+                            <a href="#" class="btn btn-outline-danger btn-sm mx-1">Deletar</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
         </painel>
     </pagina>
 @endsection
