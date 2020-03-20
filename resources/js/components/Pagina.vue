5b53ddc5a1a4dda8746c070170045a674565a824
leandro-web <leandro-web@hotmail.com>
2020-03-20 17:23:02 -0300

    Componente página

---
+++ resources/js/components/Pagina.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div v-bind:class="defineTamanho">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['tamanho'],
+        computed:{
+            defineTamanho: function(){
+                if(this.tamanho >= 12){
+                    return "col-md-12";
+                }
+                else if (this.tamanho <= 4) {
+                    return "col-md-4";
+                } else {
+                    return "col-md-"+this.tamanho;
+                }
+            }
+        }
+    }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <painel titulo="Dashboard">
-                    Teste de conteúdo...
-                    <div class="row">
-                        <div class="col-md-4">
-                            <caixa qtd="80" titulo="Artigos" url="#" cor="orange" icone="ion ion-document"></caixa>
-                        </div>
-                        <div class="col-md-4">
-                            <caixa qtd="1500" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker"></caixa>
-                        </div>
-                        <div class="col-md-4">
-                            <caixa qtd="3" titulo="Autores" url="#" cor="red" icone="ion ion-compose"></caixa>
-                        </div>
-                        <!--
-                        <div class="col-md-4">
-                            <painel titulo="Painel 3" cor="bg-warning">
-                                Teste de conteúdo C
-                            </painel>
-                        </div> 
-                        -->
-                    </div>
-                </painel>
+    <pagina tamanho="11">
+        <painel titulo="Dashboard">
+            Teste de conteúdo...
+            <div class="row">
+                <div class="col-md-4">
+                    <caixa qtd="80" titulo="Artigos" url="#" cor="orange" icone="ion ion-document"></caixa>
+                </div>
+                <div class="col-md-4">
+                    <caixa qtd="1500" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker"></caixa>
+                </div>
+                <div class="col-md-4">
+                    <caixa qtd="3" titulo="Autores" url="#" cor="red" icone="ion ion-compose"></caixa>
+                </div>
+                <!--
+                <div class="col-md-4">
+                    <painel titulo="Painel 3" cor="bg-warning">
+                        Teste de conteúdo C
+                    </painel>
+                </div> 
+                -->
             </div>
-        </div>
-    </div>
+        </painel>
+    </pagina>
 @endsection
