a1a51a02db48f5e2e600d9fb24813929b65486c8
leandro-web <leandro-web@hotmail.com>
2020-03-20 17:47:39 -0300

    Criando a view para lista de artigos

---
+++ resources/views/admin/artigos/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <pagina tamanho="12">
+        <painel titulo="Lista de artigos">
+            Teste de conteúdo...
+            
+        </painel>
+    </pagina>
+@endsection
