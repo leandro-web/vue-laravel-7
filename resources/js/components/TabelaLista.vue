414832ef6348ab632cfba41b9179bc757c239696
leandro-web <leandro-web@hotmail.com>
2020-03-21 15:52:23 -0300

    Componente TabelaLista - itens

---
+++ resources/js/components/TabelaLista.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+         <a href="#" class="btn btn-success my-3">Criar</a>
+        <table class="table table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col" v-for="titulo in titulos">{{titulo}}</th>
+                    <th scope="col">Ação</th>
+                </tr>
+            </thead> 
+            <tbody >
+                <tr v-for="item in itens">
+                    <td v-for="i in item">{{i}}</td>
+                    <td>
+                        <a href="#" class="btn btn-outline-success btn-sm mx-1">Detalhe</a>
+                        <a href="#" class="btn btn-outline-primary btn-sm mx-1">Editar</a>
+                        <a href="#" class="btn btn-outline-danger btn-sm mx-1">Deletar</a>
+                    </td>
+                </tr>
+                
+                
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['titulos','itens']
+    }
+</script>
--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -3,57 +3,17 @@
 @section('content')
     <pagina tamanho="12">
         <painel titulo="Lista de artigos">
-            
-            <a href="#" class="btn btn-success my-3">Criar</a>
-
-            <table class="table table-striped table-hover">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Título</th>
-                        <th scope="col">Descrição</th>
-                        <th scope="col">Autor</th>
-                        <th scope="col">Data</th>
-                        <th scope="col">Ação</th>
-                    </tr>
-                </thead>
-                <tbody >
-                    <tr>
-                        <td>1</td>
-                        <td>Título...</td>
-                        <td>Descrição...</td>
-                        <td>Leandro</td>
-                        <td>20/03/2020</td>
-                        <td>
-                            <a href="#" class="btn btn-outline-primary btn-sm mx-1">Editar</a>
-                            <a href="#" class="btn btn-outline-danger btn-sm mx-1">Deletar</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Título...</td>
-                        <td>Descrição...</td>
-                        <td>Leandro</td>
-                        <td>20/03/2020</td>
-                        <td>
-                            <a href="#" class="btn btn-outline-primary btn-sm mx-1">Editar</a>
-                            <a href="#" class="btn btn-outline-danger btn-sm mx-1">Deletar</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Título...</td>
-                        <td>Descrição...</td>
-                        <td>Leandro</td>
-                        <td>20/03/2020</td>
-                        <td>
-                            <a href="#" class="btn btn-outline-primary btn-sm mx-1">Editar</a>
-                            <a href="#" class="btn btn-outline-danger btn-sm mx-1">Deletar</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
+           <tabela-lista 
+                v-bind:titulos="['id','Título','Descrição',]"
+                v-bind:itens="[
+                    ['1','PHP','Curso de Laravel'],
+                    ['2','JS','Curso de VUE'],
+                    ['3','Mobile','Curso de React Native'],
+                    ['4','NODE','Curso de NodeJS'],
+                    ['5','JS','Curso de ReactJS']
+                ]"
+            >
+           </tabela-lista>
         </painel>
     </pagina>
 @endsection
